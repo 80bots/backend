22cd19af9d5af57d054841e837f733a6c20337d1
techno <prachi@technostacks.com>
2019-03-18 17:41:58 +0530

    [Fix] - migration nullable field

--- database/migrations/2019_03_13_065637_create_notifications_table.php
+++ database/migrations/2019_03_13_065637_create_notifications_table.php
@@ -25,7 +25,7 @@ class CreateNotificationsTable extends Migration
             $table->enum('push_status', ['queued','sent','not-required'])->default('queued');
             $table->enum('email_status', ['queued','sent','not-required'])->default('queued');
             $table->enum('sms_status', ['queued','sent','not-required'])->default('queued');
-            $table->timestamp('instance_stop_time');
+            $table->timestamp('instance_stop_time')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
 
