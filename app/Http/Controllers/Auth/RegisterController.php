e952cd9b105d59ead52ae0d71cf33029cf1f0aa6
Darshan <darshan.technostacks@gmail.com>
2019-03-28 13:07:40 +0530

    [New] - AwsCli config on terminal [New] - Instance Launch with bot and run startup script after launch instance [Fix] - default User credit score is (8)

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -77,6 +77,7 @@ class RegisterController extends Controller
         $user->password = bcrypt($data['password']);
         $user->verification_token = str_random();
         $user->role_id = 2;
+        $user->credit_score = 8;
         if($user->save()){
            $sendMail = $user->sendMail($user);
         }
