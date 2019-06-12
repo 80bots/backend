0c089725a43d9ef963ed4dd19f0d868bcbf5db3f
Darshan <darshan.technostacks@gmail.com>
2019-06-12 18:55:09 +0530

    [New] - working on design that provided by client for the scheduler

--- resources/views/user/instance/index.blade.php
+++ resources/views/user/instance/index.blade.php
@@ -58,14 +58,96 @@ Instance Listing
                                 <div class="col-md-2 col-sm-2">
                                     {{!empty($instance->created_at) ? date('Y-m-d', strtotime($instance->created_at)) : ''}}
                                 </div>
-                                <div class="col-md-2 col-sm-2">
+                                <div class="col-md-1 col-sm-1">
                                     <a href="{{!empty($instance->aws_public_ip) ? 'http://'.$instance->aws_public_ip : ''}}" target="_blank"><i class="fa fa-eye"></i></a>
+                                </div>
+                                <div class="col-md-1 col-sm-1">
+                                    @php
+                                        $bot_name = isset($instance->bots->bot_name)?$instance->bots->bot_name:'';
+                                    @endphp
+                                    <a href="javascript:void(0)" data-toggle="modal" data-target="#create-scheduler"
+                                       onclick="SetBotName({{$bot_name}})" class="badge badge-primary font-size-16"><i class="fa fa-pen"></i></a>
                                 </div>
                             </div>
                         </div>
                 @endforeach
             </div>
         @endif
+    </div>
+
+    <div class="modal fade" id="create-scheduler" role="dialog">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <form id="CreateSchedulerForm" action="" method="post">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title">Create Scheduler For <span id="bot-name"></span></h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-5 offset-2">
+                                Start Time
+                            </div>
+                            <div class="col-sm-5 align-items-center">
+                                End Time
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    <select name="day[]" id="" class="form-control">
+                                        <option value="">Day</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <select name="start_time[]" id="" class="form-control">
+                                        <option value="">-Select-</option>
+                                        <option value="">1:00</option>
+                                        <option value="">2:00</option>
+                                        <option value="">3:00</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    <select name="start_aside[]" id="" class="form-control">
+                                        <option value="">-Select-</option>
+                                        <option value="">AM</option>
+                                        <option value="">PM</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <select name="end_time[]" id="" class="form-control">
+                                        <option value="">-Select-</option>
+                                        <option value="">1:00</option>
+                                        <option value="">2:00</option>
+                                        <option value="">3:00</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-sm-2">
+                                <div class="form-group">
+                                    <select name="end_aside[]" id="" class="form-control">
+                                        <option value="">-Select-</option>
+                                        <option value="">AM</option>
+                                        <option value="">PM</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="submit" id="launch-inspection-submit-btn" class="btn btn-success" value="Ok">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -75,6 +157,11 @@ Instance Listing
         $(document).ready(function() {
             $('#instance-list').DataTable();
         });
+
+        function SetBotName(name){
+            console.log(name);
+            $('#bot-name').text(name);
+        }
 
         $(document).on('change', '.instStatus', function () {
             var status = $(this).val();
