566210f514407a4ad84506e77f35b8a13cd451a4
Palash <palash@itobuz.com>
2019-07-03 18:09:58 +0530

    Scheduler start currentTime change

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -142,7 +142,8 @@ class AppController extends Controller
                         if (!empty($UserInstanceObj) && !empty($detail->cron_data)) {
                             $CronDate = explode(' ', $detail->cron_data);
                             $currentTime = strtotime(date('D H:i A'));
-                            $cronTime = strtotime($CronDate[0] . $CronDate[1] . $CronDate[2]);
+                            $cronTimeDate = date_create($CronDate[0] . $CronDate[1] . $CronDate[2], timezone_open('GMT'.$CronDate[3]));
+                            $cronTime = date_timestamp_get($cronTimeDate);
                             if ($currentTime == $cronTime) {
                                 array_push($instancesIds, $UserInstanceObj->aws_instance_id);
                             }
