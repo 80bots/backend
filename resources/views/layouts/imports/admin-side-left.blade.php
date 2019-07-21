2835264eefc696d56c51814303e8ffd7bc08626a
Francis Kim <hello@franciskim.co>
2019-07-21 21:27:22 +1000

    remove sidebar-header

--- resources/views/layouts/imports/admin-side-left.blade.php
+++ resources/views/layouts/imports/admin-side-left.blade.php
@@ -1,48 +1,64 @@
 <div class="sidebar">
     <div class="">
-        <a href="#" class="sidebar-brand text-decoration-none"><img src="{{ asset('assets/images/80bots.svg') }}" alt=""></a>
+        <a href="#" class="sidebar-brand text-decoration-none"><img src="{{ asset('assets/images/80bots.svg') }}"
+                                                                    alt=""></a>
         {{--<a href="{{route('user.dashboard')}}" class="sidebar-brand text-decoration-none">80bots</a>--}}
     </div>
     <div class="sidebar-nav">
         <ul class="list-unstyled mb-0">
-            <li class="sidebar-header">Navigation</li>
             <li class="sidebar-item">
                 <ul class="sidebar-dropdown list-unstyled">
-                    <li class="sidebar-item "><a href="{{route('admin.user.index')}}" class="sidebar-link {{ (request()->is('admin/user')) ? 'active' : '' }}">Users List</a></li>
-                    <li class="sidebar-item "><a href="{{route('admin.instance.running')}}" class="sidebar-link {{ (request()->is('admin/instance/running')) ? 'active' : '' }}">Running Bots</a></li>
-                    <li class="sidebar-item "><a href="{{route('admin.bots.index')}}" class="sidebar-link {{ (request()->is('admin/bots')) ? 'active' : '' }}">All Bots</a></li>
-                    <li class="sidebar-item "><a href="{{route('admin.listsessions')}}" class="sidebar-link {{ (request()->is('admin/list-sessions')) ? 'active' : '' }}">Bot Sessions</a></li>
+                    <li class="sidebar-item "><a href="{{route('admin.user.index')}}"
+                                                 class="sidebar-link {{ (request()->is('admin/user')) ? 'active' : '' }}">Users
+                            List</a></li>
+                    <li class="sidebar-item "><a href="{{route('admin.instance.running')}}"
+                                                 class="sidebar-link {{ (request()->is('admin/instance/running')) ? 'active' : '' }}">Running
+                            Bots</a></li>
+                    <li class="sidebar-item "><a href="{{route('admin.bots.index')}}"
+                                                 class="sidebar-link {{ (request()->is('admin/bots')) ? 'active' : '' }}">All
+                            Bots</a></li>
+                    <li class="sidebar-item "><a href="{{route('admin.listsessions')}}"
+                                                 class="sidebar-link {{ (request()->is('admin/list-sessions')) ? 'active' : '' }}">Bot
+                            Sessions</a></li>
                 </ul>
             </li>
             <li class="sidebar-item">
-                    <span data-toggle="collapse" data-target="#plan" class="sidebar-link {{ (request()->is('admin/plan*')) ? '' : 'collapsed' }}"
+                    <span data-toggle="collapse" data-target="#plan"
+                          class="sidebar-link {{ (request()->is('admin/plan*')) ? '' : 'collapsed' }}"
                           aria-expanded="{{ (request()->is('admin/plan*')) ? 'true' : 'false' }}">
                         <span class="align-middle">Subscription Plan</span>
                     </span>
                 <div id="plan" class="collapse in {{ (request()->is('admin/plan*')) ? 'show' : '' }}">
                     <ul class="sidebar-dropdown list-unstyled">
-                        <li class="sidebar-item "><a href="{{route('admin.plan.index')}}" class="sidebar-link {{ (request()->is('admin/plan')) ? 'active' : '' }}">List</a></li>
-                        <!--li class="sidebar-item "><a href="{{route('admin.plan.create')}}" class="sidebar-link {{ (request()->is('admin/plan/create')) ? 'active' : '' }}">Create</a></li-->
+                        <li class="sidebar-item "><a href="{{route('admin.plan.index')}}"
+                                                     class="sidebar-link {{ (request()->is('admin/plan')) ? 'active' : '' }}">List</a>
+                        </li>
+                    <!--li class="sidebar-item "><a href="{{route('admin.plan.create')}}" class="sidebar-link {{ (request()->is('admin/plan/create')) ? 'active' : '' }}">Create</a></li-->
                     </ul>
                 </div>
             </li>
             <li class="sidebar-item">
-                    <span data-toggle="collapse" data-target="#percentage" class="sidebar-link {{ (request()->is('admin/creditPercent')) ? '' : 'collapsed' }}"
+                    <span data-toggle="collapse" data-target="#percentage"
+                          class="sidebar-link {{ (request()->is('admin/creditPercent')) ? '' : 'collapsed' }}"
                           aria-expanded="{{ (request()->is('admin/creditPercent*')) ? 'true' : 'false' }}">
                         <span class="align-middle">Low Credit Notification</span>
                     </span>
                 <div id="percentage" class="collapse in {{ (request()->is('admin/percent*')) ? 'show' : '' }}">
                     <ul class="sidebar-dropdown list-unstyled">
 
-                        <li class="sidebar-item "><a href="{{route('admin.percent.index')}}" class="sidebar-link {{ (request()->is('admin/percent')) ? 'active' : '' }}">List</a></li>
+                        <li class="sidebar-item "><a href="{{route('admin.percent.index')}}"
+                                                     class="sidebar-link {{ (request()->is('admin/percent')) ? 'active' : '' }}">List</a>
+                        </li>
 
-                        <li class="sidebar-item "><a href="{{route('admin.percent.create')}}" class="sidebar-link {{ (request()->is('admin/percent/create')) ? 'active' : '' }}">Create</a></li>
+                        <li class="sidebar-item "><a href="{{route('admin.percent.create')}}"
+                                                     class="sidebar-link {{ (request()->is('admin/percent/create')) ? 'active' : '' }}">Create</a>
+                        </li>
                     </ul>
                 </div>
             </li>
-            {{--<li class="sidebar-item "><a href="{{route('user.instance.index')}}" class="sidebar-link {{ (request()->is('user/instance')) ? 'active' : '' }}">My Bots</a></li>--}}
+        {{--<li class="sidebar-item "><a href="{{route('user.instance.index')}}" class="sidebar-link {{ (request()->is('user/instance')) ? 'active' : '' }}">My Bots</a></li>--}}
 
-            <!-- END -->
+        <!-- END -->
 
         </ul>
     </div>
--- resources/views/layouts/imports/side-left.blade.php
+++ resources/views/layouts/imports/side-left.blade.php
@@ -1,11 +1,11 @@
 <div class="sidebar">
     <div class="">
         {{--<a href="{{route('user.dashboard')}}" class="sidebar-brand text-decoration-none active">80bots</a>--}}
-        <a href="#" class="sidebar-brand text-decoration-none active"><img src="{{ asset('assets/images/80bots.svg') }}" alt=""></a>
+        <a href="#" class="sidebar-brand text-decoration-none active"><img src="{{ asset('assets/images/80bots.svg') }}"
+                                                                           alt=""></a>
     </div>
     <div class="sidebar-nav">
         <ul class="list-unstyled mb-0">
-            <li class="sidebar-header">Navigation</li>
             {{--<li class="sidebar-item ">
                     <span data-toggle="collapse" data-target="#userInstance" class="sidebar-link {{ (request()->is('user/instance*')) ? '' : 'collapsed' }}"
                           aria-expanded="{{ (request()->is('user/instance*')) ? 'true' : 'false' }}">
@@ -20,10 +20,16 @@
 
             <li class="sidebar-item">
                 <ul class="sidebar-dropdown list-unstyled">
-                    <li class="sidebar-item "><a href="{{route('user.instance.index')}}" class="sidebar-link {{ (request()->is('user/instance')) ? 'active' : '' }}">My Bots</a></li>
-                    <li class="sidebar-item "><a href="{{route('user.bots.list')}}" class="sidebar-link {{ (request()->is('user/bots')) ? 'active' : '' }}">Bots List</a></li>
+                    <li class="sidebar-item "><a href="{{route('user.instance.index')}}"
+                                                 class="sidebar-link {{ (request()->is('user/instance')) ? 'active' : '' }}">My
+                            Bots</a></li>
+                    <li class="sidebar-item "><a href="{{route('user.bots.list')}}"
+                                                 class="sidebar-link {{ (request()->is('user/bots')) ? 'active' : '' }}">Bots
+                            List</a></li>
 
-                    <li class="sidebar-item "><a href="{{route('user.scheduling.index')}}" class="sidebar-link {{ (request()->is('user/scheduling')) ? 'active' : '' }}">Scheduling List</a></li>
+                    <li class="sidebar-item "><a href="{{route('user.scheduling.index')}}"
+                                                 class="sidebar-link {{ (request()->is('user/scheduling')) ? 'active' : '' }}">Scheduling
+                            List</a></li>
 
                     {{--<li class="sidebar-item "><a href="{{route('user.scheduling.create')}}" class="sidebar-link {{ (request()->is('user/scheduling/create')) ? 'active' : '' }}">Create Scheduling</a></li>--}}
                     {{--<li class="sidebar-item "><a href="{{route('user.listsessions')}}" class="sidebar-link {{ (request()->is('user/list-sessions')) ? 'active' : '' }}">Bot Sessions</a></li>--}}
@@ -32,7 +38,9 @@
 
             <li class="sidebar-item">
                 <ul class="sidebar-dropdown list-unstyled">
-                    <li class="sidebar-item"><a href="{{route('user.subscription-plans.index')}}" class="sidebar-link {{ (request()->is('user/subscription-plans')) ? 'active' : '' }}">Subscription Plans</a></li>
+                    <li class="sidebar-item"><a href="{{route('user.subscription-plans.index')}}"
+                                                class="sidebar-link {{ (request()->is('user/subscription-plans')) ? 'active' : '' }}">Subscription
+                            Plans</a></li>
                 </ul>
             </li>
         </ul>
