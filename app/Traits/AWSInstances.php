39b37f17e9ff37f7d7137ef8bac3f8892b9e7518
Ravi <webartisan.ravi@gmail.com>
2019-07-23 11:56:08 +0100

    Default user Id added for the sync

--- app/Traits/AWSInstances.php
+++ app/Traits/AWSInstances.php
@@ -17,6 +17,7 @@ trait AWSInstances
         $result = $ec2Client->describeInstances();
         $reservations = $result->get('Reservations');
 
+
         $instancesByStatus = [];
         foreach ($reservations as $reservation) {
             $instances = $reservation['Instances'];
@@ -39,13 +40,15 @@ trait AWSInstances
                             'name'                    => $name,
                             'aws_instance_id'         => $instance['InstanceId'],
                             'aws_ami_id'              => $instance['ImageId'],
-                            'aws_security_group_id'   => $instance['SecurityGroups'][0]['GroupId'],
-                            'aws_security_group_name' => $instance['SecurityGroups'][0]['GroupName'],
-                            'aws_public_ip'           => $instance['PublicIpAddress'],
-                            'aws_public_dns'          => $instance['PublicDnsName'],
+                            'aws_security_group_id'   => isset($instance['SecurityGroups']) && count($instance['SecurityGroups']) ? $instance['SecurityGroups'][0]['GroupId'] : null,
+                            'aws_security_group_name' => isset($instance['SecurityGroups']) && count($instance['SecurityGroups']) ? $instance['SecurityGroups'][0]['GroupName'] : null,
+                            'aws_public_ip'           => $instance['PublicIpAddress'] ?? null,
+                            'aws_public_dns'          => $instance['PublicDnsName'] ?? null,
                             'created_at'              => date('Y-m-d H:i:s', strtotime($instance['LaunchTime']))
                         ];
+
                     } catch (\Exception $e) {
+                        \Log::info($e->getMessage());
                         \Log::info('An error occurred while syncing '. $instance['InstanceId']);
                     }
 
