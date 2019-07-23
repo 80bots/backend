ae83896fc4afa526c3705af6e1a69f9ff7610c01
Francis Kim <hello@franciskim.co>
2019-07-23 23:21:04 +1000

    button styling

--- resources/views/admin/bots/list.blade.php
+++ resources/views/admin/bots/list.blade.php
@@ -8,7 +8,7 @@
 @endsection
 
 @section('content')
-    <div class="wrapper" >
+    <div class="wrapper">
         @include('includes.banner')
         @forelse($platforms as $platform)
             <div class="mb-3 p-3 bg-white rounded shadow-sm">
@@ -31,21 +31,22 @@
                             </div>
                             <div class="col-md-3 col-sm-12">
                                 @foreach($bot->botTags as $botTag)
-                                  @if($tag = $botTag->tags)
-                                    <span class="badge badge-pill badge-info font-size-16 mr-1 mb-1">
+                                    @if($tag = $botTag->tags)
+                                        <span class="badge badge-pill badge-info font-size-16 mr-1 mb-1">
                                         {{ $tag->name }}
                                     </span>
-                                  @endif
+                                    @endif
                                 @endforeach
                             </div>
                             <div class="col-md-2 col-sm-12 text-right">
-                              <a href="javascript:void(0)" onclick="launchInstance({{$bot->id}});"
-                                class="badge badge-primary font-size-16" data-id="{{$bot->id}}">Launch</a>
+                                <a href="javascript:void(0)" onclick="launchInstance({{$bot->id}});"
+                                   class="btn" data-id="{{$bot->id}}">Launch <i class="fa fa-rocket"
+                                                                                aria-hidden="true"></i></a>
                             </div>
                         </div>
                     </div>
                 @empty
-                  <h3>No bots found!</h3>
+                    <h3>No bots found!</h3>
                 @endforelse
                 <small class="d-block text-right mt-3">
                     <a href="{{route('admin.bots.all.list',['id' => $platform->id])}}">All Bots</a>
@@ -59,78 +60,78 @@
 @endsection
 
 @section('script')
-<script src="{{asset('js/jquery.validate.min.js')}}" type="text/javascript"></script>
-<script type="text/javascript" src="{{ asset('js/moment.min.js')}}"></script>
+    <script src="{{asset('js/jquery.validate.min.js')}}" type="text/javascript"></script>
+    <script type="text/javascript" src="{{ asset('js/moment.min.js')}}"></script>
 
-<script>
+    <script>
 
-    $(document).ready(function(){
-        checkBotIdInQueue();
-    });
+        $(document).ready(function () {
+            checkBotIdInQueue();
+        });
 
-    function launchInstance(botId) {
-      $('#launch-instance').modal('show');
-      $('[name="bot_id"]').val(botId);
-    }
+        function launchInstance(botId) {
+            $('#launch-instance').modal('show');
+            $('[name="bot_id"]').val(botId);
+        }
 
-    $(document).on('click', '#launch-inspection-submit-btn', function () {
-        $.ajax({
-            url : "/admin/storeSession",
-            type : "POST",
-            beforeSend: function() {
-              $('#launch-inspection-submit-btn').attr('disabled', true);
-            },
-            data : {
-                _token : function () {
-                    return '{{csrf_token()}}';
+        $(document).on('click', '#launch-inspection-submit-btn', function () {
+            $.ajax({
+                url: "/admin/storeSession",
+                type: "POST",
+                beforeSend: function () {
+                    $('#launch-inspection-submit-btn').attr('disabled', true);
                 },
-                user_id : '{{ Auth::id() }}',
-                bot_id : $('[name="bot_id"]').val(),
-            },
-            success : function(response){
-                if(response.type === 'success'){
-                    window.location = "/admin/bots/mine";
-                }
-                $('[name="bot_id"]').val('');
-                $('#launch-instance').modal('hide');
-                $('#launch-inspection-submit-btn').removeAttr('disabled');
+                data: {
+                    _token: function () {
+                        return '{{csrf_token()}}';
+                    },
+                    user_id: '{{ Auth::id() }}',
+                    bot_id: $('[name="bot_id"]').val(),
+                },
+                success: function (response) {
+                    if (response.type === 'success') {
+                        window.location = "/admin/bots/mine";
+                    }
+                    $('[name="bot_id"]').val('');
+                    $('#launch-instance').modal('hide');
+                    $('#launch-inspection-submit-btn').removeAttr('disabled');
 
-            },
-            error : function(response){
-              console.log(response);
-              alert('Something went wrong!');
-              $('#launch-inspection-submit-btn').removeAttr('disabled');
-            }
+                },
+                error: function (response) {
+                    console.log(response);
+                    alert('Something went wrong!');
+                    $('#launch-inspection-submit-btn').removeAttr('disabled');
+                }
+            });
+
         });
 
-    });
-
-    function checkBotIdInQueue(){
-        $.ajax({
-            url : "/admin/checkBotIdInQueue",
-            type : "POST",
-            data : {
-                _token : function () {
-                    return '{{csrf_token()}}';
+        function checkBotIdInQueue() {
+            $.ajax({
+                url: "/admin/checkBotIdInQueue",
+                type: "POST",
+                data: {
+                    _token: function () {
+                        return '{{csrf_token()}}';
+                    }
+                },
+                success: function (response) {
+                    if (response.type === 'success') {
+                        // console.log(response);
+                        // if(response.data !== undefined && response.data.length) {
+                        //     let $botWrapper = $('#dvBotWrapper');
+                        //     for(let eachData of response.data) {
+                        //         $botWrapper.find('[data-id="'+eachData+'"]').attr('data-target','').prepend('<i class="fa fa-spinner fa-spin"></i>');
+                        //     }
+                        // }
+                    }
+                },
+                error: function (response) {
+                    console.log(response);
+                    alert('Something went wrong!');
                 }
-            },
-            success : function(response){
-                if(response.type === 'success'){
-                    // console.log(response);
-                    // if(response.data !== undefined && response.data.length) {
-                    //     let $botWrapper = $('#dvBotWrapper');
-                    //     for(let eachData of response.data) {
-                    //         $botWrapper.find('[data-id="'+eachData+'"]').attr('data-target','').prepend('<i class="fa fa-spinner fa-spin"></i>');
-                    //     }
-                    // }
-                }
-            },
-            error : function(response){
-              console.log(response);
-              alert('Something went wrong!');
-            }
-        });
+            });
 
-    }
-</script>
+        }
+    </script>
 @endsection
