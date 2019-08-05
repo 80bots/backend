8b9df34f8c4f75594c4f2b5fa547bbc7e0792533
Sergey Koval <s.sergeykoval@gmail.com>
2019-08-05 17:06:22 +0300

    Added cors middleware for API requests from different host;

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request  $request
+     * @param Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        header("Access-Control-Allow-Origin: *");
+        $headers = [
+            'Access-Control-Allow-Methods' => 'POST,GET,OPTIONS,PUT,DELETE',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+        ];
+        if ($request->getMethod() == "OPTIONS"){
+            return response()->json('OK',200, $headers);
+        }
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
+        return $response;
+    }
+}
