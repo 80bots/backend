03346da8da66974338f5fc657e61c230d0ff09a8
Sergey Koval <s.sergeykoval@gmail.com>
2019-08-28 11:24:05 +0300

    Minor fixes;

--- app/Http/Resources/User/BotInstanceResource.php
+++ app/Http/Resources/User/BotInstanceResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\User;
 
+use App\BotInstance;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BotInstanceResource extends JsonResource
@@ -24,7 +25,7 @@ class BotInstanceResource extends JsonResource
             'id'                => $this->id ?? '',
             'region'            => $region->name ?? '',
             'aws_instance_id'   => $this->aws_instance_id ?? null,
-            'name'              => $this->tag_name ?? '',
+            'name'              => $details->tag_name ?? '',
             'parameters'        => $this->bot->parameters ?? '',
             'launched_at'       => $details->start_time ?? '',
             'tag_user_email'    => $this->tag_user_email ?? '',
@@ -32,8 +33,8 @@ class BotInstanceResource extends JsonResource
             'up_time'           => $this->up_time ?? 0,
             'temp_up_time'      => $this->temp_up_time ?? 0,
             'cron_up_time'      => $this->cron_up_time ?? 0,
-            'status'            => $this->status ?? 0,
-            'ip'                => $this->aws_public_ip ?? '',
+            'status'            => $this->aws_status ?? BotInstance::STATUS_TERMINATED,
+            'ip'                => $details->aws_public_ip ?? '',
             'is_in_queue'       => $this->is_in_queue ?? 0,
         ];
     }
