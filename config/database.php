a9dccfc374a08e56fec51ffc98577b0c9fe19372
mikemitrofanov <mike.mitrofanov.dev@gmail.com>
2019-11-04 16:22:51 +0200

    Fixed config's structure

--- config/database.php
+++ config/database.php
@@ -95,30 +95,27 @@ return [
             'options' => [
                 'ssl' => true,
             ],
+//            'driver_options' => [
+//                'context' => [
+//                    'ssl' => [
+//                        'cafile' => storage_path(env('MONGO_RDS_PEM', 'rds-combined-ca-bundle.pem')),
+//                        'allow_self_signed' => true,
+//                        'verify_peer' => true,
+//                        'verify_peer_name' => true,
+//                        'verify_expiry' => true,
+//                    ]
+//                ]
+//            ],
             'driver_options' => [
-                'context' => [
-                    'ssl' => [
-                        'cafile' => storage_path(env('MONGO_RDS_PEM', 'rds-combined-ca-bundle.pem')),
-                        'allow_self_signed' => true,
+                'context' => stream_context_create([
+                    "ssl" => [
+                        "cafile" => storage_path(env('MONGO_RDS_PEM', 'rds-combined-ca-bundle.pem')),
+                        'capture_peer_cert' => true,
                         'verify_peer' => true,
                         'verify_peer_name' => true,
-                        'verify_expiry' => true,
-                    ]
-                ]
-            ]
-//            'options' => [
-//                'ssl' => true,
-//                'driver_options' => [
-//                    'context' => stream_context_create([
-//                        "ssl" => [
-//                            "cafile" => storage_path(env('MONGO_RDS_PEM', 'rds-combined-ca-bundle.pem')),
-//                            'capture_peer_cert' => true,
-//                            'verify_peer' => true,
-//                            'verify_peer_name' => true,
-//                            'allow_self_signed' => false,
-//                        ]])
-//                ],
-//            ],
+                        'allow_self_signed' => false,
+                    ]])
+            ],
         ],
 
     ],
