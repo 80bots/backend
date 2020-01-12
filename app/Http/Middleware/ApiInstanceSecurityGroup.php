4db09dd4255aa4fcf20b9e34193fd69a55f1519d
Francis Kim <hello@franciskim.co>
2020-01-12 19:53:48 +1100

    more debugging logs

--- app/Http/Middleware/ApiInstanceSecurityGroup.php
+++ app/Http/Middleware/ApiInstanceSecurityGroup.php
@@ -6,28 +6,30 @@ use App\Jobs\UpdateInstanceSecurityGroup;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class ApiInstanceSecurityGroup
 {
     /**
      * Handle an incoming request.
      *
-     * @param Request  $request
-     * @param Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
+        Log::info("Starting ApiInstanceSecurityGroup");
         if (Auth::check()) {
-
-            $user   = Auth::user();
-            $ip     = $request->ip();
+            Log::info("ApiInstanceSecurityGroup 2");
+            $user = Auth::user();
+            $ip = $request->ip();
 
             $visitors = $user->visitors->map(function ($item, $key) {
                 return $item['ip'] ?? null;
             })->toArray();
 
-            if (! in_array($ip, $visitors)) {
+            if (!in_array($ip, $visitors)) {
                 dispatch(new UpdateInstanceSecurityGroup($user, $ip));
             }
         }
