813c43b2c97777286ca5bb8f48784c270b74fe7c
Francis Kim <francis@inforca.com>
2020-04-14 12:47:47 +1000

    restore cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,21 +15,20 @@ return [
     |
     */
 
-    // commented out since the CORS header was doubling up
-//    'paths' => ['api/*'],
-//
-//    'allowed_methods' => ['*'],
-//
-//    'allowed_origins' => ['*'],
-//
-//    'allowed_origins_patterns' => [],
-//
-//    'allowed_headers' => ['*'],
-//
-//    'exposed_headers' => [],
-//
-//    'max_age' => 0,
-//
-//    'supports_credentials' => false,
+    'paths' => ['api/*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 
 ];
