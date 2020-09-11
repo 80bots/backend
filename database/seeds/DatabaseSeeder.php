7bce4ea3ca5fcc8ef427eaf6ee1a05a169423887
romfis <roman.bikbaiev.dev@gmail.com>
2020-09-11 16:51:10 +0300

    Added demo-bot.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
          $this->call([
              AwsRegionsTableSeeder::class,
              TimezonesTableSeeder::class,
+             DemoBotSeeder::class,
              BotsTableSeeder::class,
              AwsAmisTableSeeder::class,
              AwsSettingsTableSeeder::class,
